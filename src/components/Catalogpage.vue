9c4bf0ce01b53c22f8a081bb173ebdbc7892502f
Maxim Alekseev <inquisi70r@Maxims-MacBook-Air.local>
2021-11-17 17:21:04 +0300

    Initial commit

---
+++ src/components/Catalogpage.vue
@@ -0,0 +1,32 @@
+<template>
+  <header>
+    <div>
+      <ul>
+        <li><a>Главная</a></li>
+        <li><a>Каталок</a></li>
+        <li><a>О сервисе</a></li>
+
+      </ul>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Homepage',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+header {
+  margin: 10px auto;
+  display: -webkit-flex;
+  display: -ms-flex;
+  display: flex;
+  align-items: center;
+}
+</style>
